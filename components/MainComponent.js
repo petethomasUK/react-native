e91b4f558a7db22f5089084fc9d38cdd38f73288
pete thomas <pete.thomas@apptitude.co.uk>
2019-12-23 21:44:44 +0000

    naivgation 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -3,28 +3,64 @@ import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import DishDetail from './DishDetailComponent';
 import { View } from 'react-native';
+import Home from './HomeComponent';
 import {createStackNavigator} from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import {createAppContainer} from 'react-navigation';
 
-const MenuNavigator = createStackNavigator({
-        Menu: { screen: Menu },
-        DishDetail: { screen: DishDetail }
+const HomeNavigator = createStackNavigator({
+  Home: { screen: Home }
+}, {
+  navigationOptions: ({ navigation }) => ({
+    headerStyle: {
+        backgroundColor: "#512DA8"
     },
-    {
-        initialRouteName: 'Menu',
-        navigationOptions: {
-            headerStyle: {
-                backgroundColor: "#512DA8"
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-                color: "#fff"            
-            }
-        }
-    }
+    headerTitleStyle: {
+        color: "#fff"            
+    },
+    headerTintColor: "#fff"  
+  })
+});
+
+const MenuNavigator = createStackNavigator({
+  Menu: { screen: Menu },
+  DishDetail: { screen: DishDetail }
+},
+{
+  initialRouteName: 'Menu',
+  navigationOptions: {
+      headerStyle: {
+          backgroundColor: "#512DA8"
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+          color: "#fff"            
+      }
+  }
+}
 );
 
-const App = createAppContainer(MenuNavigator);
+const MainNavigator = createDrawerNavigator({
+  Home: 
+    { screen: HomeNavigator,
+      navigationOptions: {
+        title: 'Home',
+        drawerLabel: 'Home'
+      }
+    },
+  Menu: 
+    { screen: MenuNavigator,
+      navigationOptions: {
+        title: 'Menu',
+        drawerLabel: 'Menu'
+      }, 
+    }
+}, {
+drawerBackgroundColor: '#D1C4E9'
+});
+
+
+const App = createAppContainer(MainNavigator);
 
 
 
